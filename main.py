fc278ebebc1ff8d0c2126a202944e7ae27b97d6b
bikupep <stepan.shmygin@mail.ru>
2026-01-11 13:31:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def generate_ring_sector(r_min, r_max, th_min, th_max, nr, nth) -> Body:
     ths = np.linspace(th_min, th_max, nth)
     for r in rs:
         for th in ths:
-            x = r * np.cos(th)
+
             y = r * np.sin(th)
             body.add_point(MaterialPoint(x, y))
     return body
@@ -19,7 +19,7 @@ def generate_ring_sector(r_min, r_max, th_min, th_max, nr, nth) -> Body:
 def main():
     # 1. Настройка физики через ЛЯМБДА-ФУНКЦИИ
     # v1 = -A(t)*x, v2 = B(t)*y
-    # Передаем конкретные зависимости прямо здесь
+    # Передаем конкретные зависимости прямо здесь   
     field = AnalyticalField(
         func_x=lambda t, x: -np.cosh(t) * x,
         func_y=lambda t, y:  np.cos(t) * y
@@ -28,7 +28,7 @@ def main():
     # 2. Выбор солвера (Полиморфизм: мы могли бы подставить EulerSolver)
     solver = RungeKutta4()
 
-    # 3. Создание сущности (Тела)
+    # 3. Создание сущности (Тела)     
     body = generate_ring_sector(1.0, 2.0, 0.05, np.pi/2 - 0.05, 5, 10)
 
     # 4. Визуализатор
@@ -44,7 +44,7 @@ def main():
         body.evolve(current_t, dt, field, solver)
         current_t += dt
 
-    # 6. Финальная визуализация
+    # 6. Финальная визуализация           
     plotter.plot_body(body, 'red', f'End (t={t_end})')
     plotter.plot_trajectories(body)
     plotter.show()
