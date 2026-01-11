c7d7a99b835da037a63872cc883ac61053a738e7
bikupep <stepan.shmygin@mail.ru>
2026-01-11 13:32:37 +0300

    Update base.py

--- simulation_project/core/base.py
+++ simulation_project/core/base.py
@@ -2,14 +2,14 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 class IVectorField(ABC):
-    """Абстрактный класс для любого векторного поля (например, поля скоростей)."""
+    """Абстрактный класс  для любого векторного поля (например, поля скоростей)."""
     @abstractmethod
     def get_velocity(self, t: float, coords: np.ndarray) -> np.ndarray:
         pass
 
 class ISolver(ABC):
-    """Абстрактный класс для численного решателя ДУ."""
+    """Абстрактный класс  для численного решателя ДУ."""
     @abstractmethod
     def step(self, func, t: float, y: np.ndarray, dt: float) -> np.ndarray:
-        """Сделать один шаг по времени."""
+        """Сделать один шаг  по времени."""
         pass
