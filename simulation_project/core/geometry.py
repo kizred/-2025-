f001ef0448f9d670270a20e41e29f373d4c3bf45
kizred <zautsewsergei@gmail.com>
2026-01-12 05:49:40 +0300

    Update geometry.py

--- simulation_project/core/geometry.py
+++ simulation_project/core/geometry.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-class Point:
-    """Описывает просто точку в  пространстве."""
-    def __init__(self, x: float, y: float):
+class Point:#точка на плоскости
+    def __init__(self, x: float, y: float):#массив x y
         self.coords = np.array([x, y], dtype=float)
 
     def __repr__(self):
         return f"Point({self.coords[0]:.2f}, {self.coords[1]:.2f})"
 
-    @property
+    @property#p.x(0) и p.x
     def x(self): return self.coords[0]
 
     @property
