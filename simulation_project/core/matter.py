31a9d11312730bea974b762419223d8e3133488d
kizred <zautsewsergei@gmail.com>
2026-01-12 06:12:16 +0300

    Update matter.py

--- simulation_project/core/matter.py
+++ simulation_project/core/matter.py
@@ -4,48 +4,36 @@ from .geometry import Point
 from .base import IVectorField, ISolver
 
 class Trajectory:
-    """Класс, отвечающий за хранение истории движения."""
+    
     def __init__(self):
-        self._history: List[np.ndarray] = []
+        self._history: List[np.ndarray] = []#список для положений точек
 
     def add_point(self, coords: np.ndarray):
-        self._history.append(coords.copy())
+        self._history.append(coords.copy())#копия координат
 
     def get_path_as_array(self) -> np.ndarray:
         return np.array(self._history)
 
-class MaterialPoint(Point):
-    """
-    Материальная точка. 
-    Наследуется от Point (имеет координаты), но добавляет траекторию.
-    """
+class MaterialPoint(Point):#наследует траекторию
     def __init__(self, x: float, y: float):
         super().__init__(x, y)
-        self.trajectory = Trajectory()
+        self.trajectory = Trajectory()#point and tr
         self.trajectory.add_point(self.coords) # Записываем начальное положение
 
-    def move(self, new_coords: np.ndarray):
-        """Обновляет координаты и сохраняет историю."""
+    def move(self, new_coords: np.ndarray):#перемещения
         self.coords = new_coords
         self.trajectory.add_point(self.coords)
 
-class Body:
-    """
-    Тело, состоящее из множества материальных точек.
-    """
+class Body:#тело
     def __init__(self):
-        self.points: List[MaterialPoint] = []
+        self.points: List[MaterialPoint] = []#список для тел
 
     def add_point(self, p: MaterialPoint):
-        self.points.append(p)
+        self.points.append(p)#+точка
 
     def evolve(self, t: float, dt: float, field: IVectorField, solver: ISolver):
-        """
-        Действие: Эволюция тела во времени.
-        Полиморфизм: метод принимает ЛЮБОЙ solver и ЛЮБОЕ field.
-        """
-        # Функция правой части ДУ для солвера: f(t, y) -> v
-        # Оборачиваем вызов метода поля в лямбду или функцию
+    
+        # лямбда
         d_dt = lambda time, y: field.get_velocity(time, y)
 
         for p in self.points:
